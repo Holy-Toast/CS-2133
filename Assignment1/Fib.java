60b08d7f4f8349d0e1f73a967b59d952cd69d69b
Holy-Toast <32134400+Holy-Toast@users.noreply.github.com>
2017-09-20 13:46:07 -0400

    Update Fib.java
    Added javadoc

--- Assignment1/Fib.java
+++ Assignment1/Fib.java
@@ -1,29 +1,36 @@
-public class Fib
-{
-  public static void main(String[] args)
-  {
+/**
+* This program calculates the Fibonacci number as given
+* by the user via the command line.
+*
+* @author Patrick Roeber
+*/
+public class Fib {
+  
+  /**
+  * This function calculates the Fibonacci number as given
+  * by the user via the command line.
+  *
+  * @param args A String array, the first element of this array
+  *             will be used to calculate the Fibonacci number
+  */
+  public static void main(String[] args) {
     int nth = 0;
     int fib_number = 1;
 
     int fib1 = 1;
     int fib2 = 1;
 
-    if(args.length < 1)
-    {
+    if(args.length < 1) {
       System.out.println("usage: Fib <number (1 - 48)>");
     }
-    else
-    {
+    else {
       nth = Integer.parseInt(args[0]);
 
-      if(nth < 1 || nth > 48)
-      {
+      if(nth < 1 || nth > 48) {
         System.out.println("usage: Fib <number (1 - 48)>");
       }
-      else
-      {
-        for(int i=3; i <= nth; i++)
-        {
+      else {
+        for(int i=3; i <= nth; i++) {
           fib_number = fib1 + fib2;
           fib2 = fib1;
           fib1 = fib_number;
