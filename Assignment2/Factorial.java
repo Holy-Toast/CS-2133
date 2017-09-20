c514a4f215541165b1a498af3755f72225134b18
Holy-Toast <32134400+Holy-Toast@users.noreply.github.com>
2017-09-20 13:18:12 -0400

    Update Factorial.java
    Added in javadoc comments.

--- Assignment2/Factorial.java
+++ Assignment2/Factorial.java
@@ -1,43 +1,54 @@
-public class Factorial
-{
-  public static void main(String[] args)
-  {
+/**
+* This class calculates the factorial of the numbers 0 
+* and 5.
+*
+* @author Patrick Roeber
+*/
+public class Factorial {
+  
+  /**
+  * Calls calculate twice, once with 0 and the second with
+  * 5, then prints out either Test Passed or Test Failed
+  * depending on whether or not the calculation was preformed
+  * correctly.
+  *
+  * @param args unused in this function
+  */
+  public static void main(String[] args) {
     long calc = calculate(0);
     System.out.print("Factorial.calculate(0) returned " + calc + ". ");
-    if(calc == 1)
-    {
+    if(calc == 1) {
       System.out.println("Test Passed!");
     }
-    else
-    {
+    else {
       System.out.println("Test Failed!");
     }
 
     calc = calculate(5);
     System.out.print("Factorial.calculate(5) returned " + calc + ". ");
-    if(calc == 120)
-    {
+    if(calc == 120) {
       System.out.println("Test Passed!");
     }
-    else
-    {
+    else {
       System.out.println("Test Failed!");
     }
   }
 
-  public static long calculate(long n)
-  {
-    if(n < 0 || n > 20)
-    {
+  /**
+  * Recursively finds the factorial of n.
+  *
+  * @param n The number of which the factorial will be
+  *          taken of
+  */
+  public static long calculate(long n) {
+    if(n < 0 || n > 20) {
       System.out.println("n must be a number between 0 and 19.");
       return -1;
     }
-    else if(n == 0)
-    {
+    else if(n == 0) {
       return 1;
     }
-    else
-    {
+    else {
       return n * calculate(n - 1);
     }
   }
