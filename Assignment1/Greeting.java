07c9e1442df82942683fb7ed3e38b5f4046699c2
Holy-Toast <32134400+Holy-Toast@users.noreply.github.com>
2017-09-20 13:46:38 -0400

    Update Greeting.java
    Fixed {}

--- Assignment1/Greeting.java
+++ Assignment1/Greeting.java
@@ -1,7 +1,5 @@
-public class Greeting
-{
-  public static void main(String[] args)
-  {
+public class Greeting {
+  public static void main(String[] args) {
     System.out.println("I can't let you do that dave");
   }
 }
