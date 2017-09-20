205f110e5002f9595f35c0cf40ab794078666883
Holy-Toast <32134400+Holy-Toast@users.noreply.github.com>
2017-09-20 13:43:51 -0400

    Update Average.java
    Added javadoc

--- Assignment1/Average.java
+++ Assignment1/Average.java
@@ -1,9 +1,23 @@
 import java.util.*;
 
-public class Average
-{
-  public static void main(String[] args)
-  {
+/**
+* This program takes input from the user until the user 
+* inputs a negative number, then it calculates the average
+* of the numbers inputted, ignoring the negative number.
+*
+* @author Patrick Roeber
+*/
+public class Average {
+  
+  /**
+  * This function takes input from the user until the user 
+  * inputs a negative number, then it calculates the average
+  * of the numbers inputted, ignoring the negative number.
+  * Then prints it to the screen.
+  *
+  * @param args A String array, unused in this program.
+  */
+  public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
     double inputNumber = 0.0;
@@ -18,19 +32,16 @@ public class Average
 
     System.out.println("Please enter a series of numbers. Enter a negative number to quit.");
 
-    while (no_negatives)
-    {
+    while (no_negatives) {
       inputNumber = input.nextDouble();
-      if (inputNumber < 0)
-      {
+      if (inputNumber < 0) {
         no_negatives = false;
         break;
       }
       inputList.add(inputNumber);
     }
 
-    for (i = 0; i < inputList.size(); i++)
-    {
+    for (i = 0; i < inputList.size(); i++) {
       total = total + inputList.get(i);
     }
     average = total / i;
