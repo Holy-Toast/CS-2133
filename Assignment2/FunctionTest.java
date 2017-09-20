712ad08e27cedb974c3e4056ef693e4e1180aadd
Holy-Toast <32134400+Holy-Toast@users.noreply.github.com>
2017-09-20 13:32:19 -0400

    Update FunctionTest.java
    Added javadoc

--- Assignment2/FunctionTest.java
+++ Assignment2/FunctionTest.java
@@ -1,10 +1,32 @@
+/**
+* This program finds the real root between 3 and 4 of
+* sin(x)
+*
+* @author Patrick Roeber
+*/
 public class FunctionTest {
+  
+  /**
+  * Calculates the root between 3 and 4 of sin(x) with a
+  * certainty of 0.00000001, then prints out the answer.
+  *
+  * @param args Input from the command line. Not used within this program.
+  */
   public static void main(String[] args) {
     double answer = 0.0;
     answer = findRoot(3, 4, 0.00000001);
     System.out.println("The answer is: " + answer);
   }
 
+  /**
+  * Calculates the root between a and b of sin(x) with a
+  * certainty of epsilon.
+  *
+  * @param a       The lower bound 
+  * @param b       The upper bound
+  * @param epsilon The certainty of the calculation
+  * @return        The real root found between a and b
+  */
   public static double findRoot(double a, double b, double epsilon) {
     double x = (a + b) / 2;
     double root = 0;
