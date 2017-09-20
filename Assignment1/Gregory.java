39eb0311d0c2dc4a60e286fa2e134cf26750bc35
Holy-Toast <32134400+Holy-Toast@users.noreply.github.com>
2017-09-20 13:50:10 -0400

    Update Gregory.java
    added javadoc

--- Assignment1/Gregory.java
+++ Assignment1/Gregory.java
@@ -1,22 +1,31 @@
-public class Gregory
-{
-  public static void main(String[] args)
-  {
+/**
+* This program emulates PI using the Gregory series.
+*
+* @author Patrick Roeber
+*/
+public class Gregory {
+  
+  /**
+  * Takes in a number from the command line, then uses the Gregory
+  * series to aproximate PI. Then prints out the result and the percent
+  * error using Math.PI as the given value.
+  *
+  * @param args A String array, the first element of which will be the
+  *             number of times to calculate the Gregory series
+  */
+  public static void main(String[] args) {
     int gregNum = 0;
     double gregPI = 0;
     double total = 0.0;
     double percErr = 0.0;
 
-    if(args.length < 1)
-    {
+    if(args.length < 1) {
       System.out.println("usage: Gregory <number>");
     }
-    else
-    {
+    else {
       gregNum = Integer.parseInt(args[0]);
 
-      for(int i=1; i <= gregNum; i++)
-      {
+      for(int i=1; i <= gregNum; i++) {
         gregPI = ( (Math.pow(-1, (i+1) ) ) / ( (2 * i) - 1) );
         total = total + gregPI;
       }
